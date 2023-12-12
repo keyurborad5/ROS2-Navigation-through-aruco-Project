9675ea719d50083ac557a2309e6efb8c8d445825
Keyur Borad <keyurborad5@gmail.com>
2023-12-11 21:26:49 -0500

    created publisher cmd val
    1.Publishing now the velocity of the bot in cmd_vel and bot moves
    2. Made subscriber of part's advance camera but not working at the moment . Hence the subscriber is commented out

--- rwa3_ws/src/group17/include/group17/my_robot_node.hpp
+++ rwa3_ws/src/group17/include/group17/my_robot_node.hpp
@@ -4,6 +4,7 @@
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <utils.hpp>
 #include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 // for static broadcaster
 #include "tf2_ros/static_transform_broadcaster.h"
 // for dynamic broadcaster
@@ -11,6 +12,7 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
 #include <ros2_aruco_interfaces/msg/aruco_markers.hpp>
+#include <mage_msgs/msg/advanced_logical_camera_image.hpp>
 using namespace std::chrono_literals;
 
 class MyRobotNode : public rclcpp::Node
@@ -41,7 +43,13 @@ public:
 
 
         //********************Subscriber**************************
-        aruco_subscriber_=this->create_subscription<ros2_aruco_interfaces::msg::ArucoMarkers>("aruco_markers",10, std::bind(&MyRobotNode::aruco_marker_sub_cb,this,std::placeholders::_1));
+        aruco_cam_subscriber_=this->create_subscription<ros2_aruco_interfaces::msg::ArucoMarkers>("aruco_markers",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::aruco_cam_sub_cb,this,std::placeholders::_1));
+        // part_cam_subscriber_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/advanced_logical_camera/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb,this,std::placeholders::_1));
+
+        //********************Publisher*****************************
+        cmd_val_publisher_=this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel",10);
+        cmd_val_timer_ = this->create_wall_timer(std::chrono::milliseconds(100),std::bind(&MyRobotNode::cmd_val_pub_cb,this));
+
 
     }
 
@@ -53,18 +61,28 @@ private:
     std::shared_ptr<tf2_ros::Buffer> aruco_tf_buffer_;
     /*!< MyRobotNode object */
     std::shared_ptr<tf2_ros::TransformBroadcaster> aruco_tf_broadcaster_;
+    /*!< Buffer that stores several seconds of transforms for easy lookup by the listener. */
+    std::shared_ptr<tf2_ros::Buffer> part_tf_buffer_;
+    /*!< MyRobotNode object */
+    std::shared_ptr<tf2_ros::TransformBroadcaster> part_tf_broadcaster_;
     /*!< Utils object to access utility functions*/
     std::shared_ptr<Utils> utils_ptr_;
-    /*!< Wall timer object for the broadcaster*/
-    rclcpp::TimerBase::SharedPtr broadcast_timer_;
-    /*!< Wall timer object for the static broadcaster*/
-    rclcpp::TimerBase::SharedPtr static_broadcast_timer_;
+    int part_color_;
+    int part_type_;
+    
+    
 
     /**
      * @brief Timer to broadcast the transform
      *
      */
     void aruco_broadcaster(ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr);
+
+    /**
+     * @brief Timer to broadcast the transform
+     *
+     */
+    void part_broadcaster(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr);
     
 
     //******************Listener***********************
@@ -75,19 +93,41 @@ private:
     std::unique_ptr<tf2_ros::Buffer> aruco_tf_listener_buffer_;
     /*!< Transform listener object */
     std::shared_ptr<tf2_ros::TransformListener> aruco_transform_listener_{nullptr};
-    /*!< Wall timer object */
-    rclcpp::TimerBase::SharedPtr listen_timer_;
-
+    
     /**
-     * @brief Listen to a transform
+     * @brief Listen to a aruco transform
      *
      * @param source_frame Source frame (child frame) of the transform
      * @param target_frame Target frame (parent frame) of the transform
      */
     void aruco_listen_transform(const std::string &source_frame, const std::string &target_frame);
 
+    /*!< Buffer that stores several seconds of transforms for easy lookup by the listener. */
+    std::unique_ptr<tf2_ros::Buffer> part_tf_listener_buffer_;
+    /*!< Transform listener object */
+    std::shared_ptr<tf2_ros::TransformListener> part_transform_listener_{nullptr};
+    
+    /**
+     * @brief Listen to a part transform
+     *
+     * @param source_frame Source frame (child frame) of the transform
+     * @param target_frame Target frame (parent frame) of the transform
+     */
+    void part_listen_transform(const std::string &source_frame, const std::string &target_frame);
+
 
     //***********Subscriber**********************
-    rclcpp::Subscription<ros2_aruco_interfaces::msg::ArucoMarkers>::SharedPtr aruco_subscriber_;
-    void aruco_marker_sub_cb(const ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr msg);
+    rclcpp::Subscription<ros2_aruco_interfaces::msg::ArucoMarkers>::SharedPtr aruco_cam_subscriber_;
+    void aruco_cam_sub_cb(const ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr msg);
+
+    rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber_;
+    void part_cam_sub_cb(const mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg);
+
+    //*******************Publisher**********************
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_val_publisher_;
+    rclcpp::TimerBase::SharedPtr cmd_val_timer_;
+    void cmd_val_pub_cb();
+
+
+   
 };
--- rwa3_ws/src/group17/src/my_robot_node.cpp
+++ rwa3_ws/src/group17/src/my_robot_node.cpp
@@ -35,6 +35,32 @@ void MyRobotNode::aruco_broadcaster(ros2_aruco_interfaces::msg::ArucoMarkers::Sh
     aruco_tf_broadcaster_->sendTransform(aruco_dynamic_transform_stamped);
     RCLCPP_INFO_STREAM(this->get_logger(), "Aruco Broadcasting_dynamic_frame : "<<msg->poses[0].position.x);
 }
+void MyRobotNode::part_broadcaster(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg)
+{
+    geometry_msgs::msg::TransformStamped part_dynamic_transform_stamped;
+
+    // RCLCPP_INFO(this->get_logger(), "Broadcasting dynamic_frame");
+    part_dynamic_transform_stamped.header.stamp = this->get_clock()->now();
+
+    part_dynamic_transform_stamped.header.frame_id = "logical_camera_link";
+    part_dynamic_transform_stamped.child_frame_id = "part";
+
+    part_color_= msg->part_poses[0].part.color;
+    part_type_= msg->part_poses[0].part.type;
+
+    part_dynamic_transform_stamped.transform.translation.x = msg->part_poses[0].pose.position.x;
+    part_dynamic_transform_stamped.transform.translation.y = msg->part_poses[0].pose.position.y;
+    part_dynamic_transform_stamped.transform.translation.z = msg->part_poses[0].pose.position.z;
+
+    //geometry_msgs::msg::Quaternion quaternion = utils_ptr_->set_quaternion_from_euler(M_PI, 0.0, M_PI / 3);
+    part_dynamic_transform_stamped.transform.rotation.x = msg->part_poses[0].pose.orientation.x;
+    part_dynamic_transform_stamped.transform.rotation.y = msg->part_poses[0].pose.orientation.y;
+    part_dynamic_transform_stamped.transform.rotation.z = msg->part_poses[0].pose.orientation.z;
+    part_dynamic_transform_stamped.transform.rotation.w = msg->part_poses[0].pose.orientation.w;
+    // Send the transform
+    part_tf_broadcaster_->sendTransform(part_dynamic_transform_stamped);
+    RCLCPP_INFO_STREAM(this->get_logger(), "PART advance cam Broadcasting_dynamic_frame : "<<msg->part_poses[0].pose.position.x;);
+}
 
 void MyRobotNode::aruco_listen_transform(const std::string &source_frame, const std::string &target_frame)
 {
@@ -64,18 +90,62 @@ void MyRobotNode::aruco_listen_transform(const std::string &source_frame, const 
     //                                                     << "qz: " << pose_out.orientation.z << "\t"
     //                                                     << "qw: " << pose_out.orientation.w << "\n");
 }
+void MyRobotNode::part_listen_transform(const std::string &source_frame, const std::string &target_frame)
+{
+    geometry_msgs::msg::TransformStamped t_stamped;
+    geometry_msgs::msg::Pose pose_out;
+    try
+    {
+        t_stamped = part_tf_listener_buffer_->lookupTransform(source_frame, target_frame, tf2::TimePointZero, 50ms);
+    }
+    catch (const tf2::TransformException &ex)
+    {
+        RCLCPP_ERROR_STREAM(this->get_logger(), "Could not get transform between " << source_frame << " and " << target_frame << ": " << ex.what());
+        return;
+    }
 
-void MyRobotNode::aruco_marker_sub_cb(ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr msg){
+    pose_out.position.x = t_stamped.transform.translation.x;
+    pose_out.position.y = t_stamped.transform.translation.y;
+    pose_out.position.z = t_stamped.transform.translation.z;
+    pose_out.orientation = t_stamped.transform.rotation;
+
+    RCLCPP_INFO_STREAM(this->get_logger(), target_frame << " in " << source_frame << ":\n"
+                                                        << "x: " << pose_out.position.x << "\t"
+                                                        << "y: " << pose_out.position.y << "\t"
+                                                        << "z: " << pose_out.position.z << "\n"
+                                                        << "qx: " << pose_out.orientation.x << "\t"
+                                                        << "qy: " << pose_out.orientation.y << "\t"
+                                                        << "qz: " << pose_out.orientation.z << "\t"
+                                                        << "qw: " << pose_out.orientation.w << "\n");
+}
+
+
+void MyRobotNode::aruco_cam_sub_cb(ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr msg){
     
     RCLCPP_INFO_STREAM(this->get_logger(),"Aruco marker callback ");
     
     aruco_broadcaster(msg);
 
-    aruco_listen_transform("odom", "aruco_mark");
+    aruco_listen_transform("odom", "aruco_mark");   
 
-
+}
+void MyRobotNode::part_cam_sub_cb(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg){
     
+    if(msg){
+    RCLCPP_INFO_STREAM(this->get_logger(),"Part Advance Camera callback : ");
 
+    // RCLCPP_INFO_STREAM(this->get_logger(),"Part Advance Camera callback ");
+    
+    part_broadcaster(msg);
+
+    part_listen_transform("odom", "logical_camera_link");   
+    }
+}
+void MyRobotNode::cmd_val_pub_cb(){
+    geometry_msgs::msg::Twist task;
+    task.linear.x = 0.1;
+    cmd_val_publisher_->publish(task);
+    RCLCPP_INFO_STREAM(this->get_logger(),"Going_forward by 0.1");
 }
 
 int main(int argc, char** argv) {
